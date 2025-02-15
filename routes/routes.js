ab0f2f7919b9c95bff99beaff2b58e17ef3f544e
sudarshanrj07 <srjalawadi07@gmail.com>
2025-02-15 16:00:36 +0530

    "Deleted duplicates"

--- routes/routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import userRoute from "./user.js";
-
-const router = Router();
-
-router.use("/user", userRoute);
-
-export default router;
--- utils/connection.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const mongoConnect = async (url) => {
-	return mongoose.connect(url);
-};
